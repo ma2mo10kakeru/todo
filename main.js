1a165f80d8a042e124ceefec76d539b2a80c9fde
ma2mo10kakeru <82557429+ma2mo10kakeru@users.noreply.github.com>
2021-05-24 14:17:47 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,4 @@
 
-// ★STEP2
-// https://jp.vuejs.org/v2/examples/todomvc.html
-var STORAGE_KEY = 'todos-vuejs-demo'
-var todoStorage = {
-  fetch: function () {
-    var todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
-    todos.forEach(function (todo, index) {
-      todo.id = index
-    })
-    todoStorage.uid = todos.length
-    return todos
-  },
-  save: function (todos) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
-  }
-}
-
-
 // ★STEP1
 new Vue({
   el: '#app',
